cc046b0ac34d0c0224f7216eabe5ac8fa9a70db5
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-23 21:46:38 +0530

    [Sunil]:Performed Ordered LinkedList

---
+++ OrderedAndUnOrdered/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderedAndUnOrdered
+{
+    internal class Node
+    {
+        public string Data;
+        public Node next;
+
+        public Node(string data)
+        {
+            this.Data = data;
+        }
+    }
+}
---
+++ OrderedAndUnOrdered/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderedAndUnOrdered
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderList order = new OrderList();
+
+            Console.WriteLine("Enter word to search");
+            string word = Console.ReadLine();
+            order.Ordered(word);
+        }
+    }
+}
